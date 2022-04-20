1dc4543d55136fee667ef11d4851a4c478e3bf2a
예준 <ohman0705@naver.com>
2022-04-20 18:14:42 +0900

    add controller class

---
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("hello")
+    public List<String> hello() {
+        return Arrays.asList("안녕하세요", "Hello");
+    }
+}
