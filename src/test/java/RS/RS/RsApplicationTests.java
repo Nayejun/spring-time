3d18142227fe00ac9bb02a8a513827e5a8a86e4b
예준 <ohman0705@naver.com>
2022-04-20 17:59:07 +0900

    initial commit

---
+++ src/test/java/RS/RS/RsApplicationTests.java
@@ -0,0 +1,13 @@
+package RS.RS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
